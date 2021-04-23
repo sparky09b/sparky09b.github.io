e81cf03500dee65bf24d367220d75b672e959369
Mark Stevens Account <markjstev@gmail.com>
2021-04-23 16:58:49 -0600

    clean up of files

---
+++ test-file/start/js/script-snowman.js
@@ -0,0 +1,5 @@
+let carrot = new Date();
+let hat = carrot.getFullYear();
+
+document.getElementById("snowman").textContent = hat;
+document.getElementById("lu").textContent = "Last Updated: " + document.lastModified;
